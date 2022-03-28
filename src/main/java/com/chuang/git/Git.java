eabbdd08eaefcf2e7e86ec2a7568e79e040cf260
chuang <401783725@qq.com>
2022-03-28 11:21:24 +0800

    second commit

--- src/main/java/com/chuang/git/Git.java
+++ src/main/java/com/chuang/git/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
 
         System.out.println("李闯大爸");
+        System.out.println("李闯二爸");
+        System.out.println("李闯三爸");
     }
 }
